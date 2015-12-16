b767431e88ef2e84349fea0306b464a26a9b4f0e
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-12-16 23:21:35 +0100

    Update the codebase to phoenix v1.1

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule Colibri.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.4"},
+    [{:phoenix, "~> 1.1"},
      {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.2"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
      {:corsica, "~> 0.4"},
