7f7d50a2a1ef5f073b9e589ed60bcbc62e9083fa
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-12-17 23:02:39 +0100

    Playlist model serializer.

--- web/views/playlist_view.ex
+++ web/views/playlist_view.ex
@@ -1,16 +1,9 @@
 defmodule Colibri.PlaylistView do
   use Colibri.Web, :view
 
-  def render("index.json", %{playlists: playlists}) do
-    %{data: render_many(playlists, Colibri.PlaylistView, "playlist.json")}
-  end
+  location "/playlists/:id"
+  attributes [:title]
 
-  def render("show.json", %{playlist: playlist}) do
-    %{data: render_one(playlist, Colibri.PlaylistView, "playlist.json")}
-  end
-
-  def render("playlist.json", %{playlist: playlist}) do
-    %{id: playlist.id,
-      title: playlist.title}
-  end
+  has_many :tracks,
+  link: "/playlists/:id/tracks"
 end
